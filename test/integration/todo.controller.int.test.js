0919001e26b583ede02bca3824947443377f7231
lmcuende <lmcuende@gmail.com>
2020-04-24 20:58:50 +0200

    express router

--- test/integration/todo.controller.int.test.js
+++ test/integration/todo.controller.int.test.js
@@ -5,10 +5,10 @@ const newTodo = require("../mock-data/new-todo.json");
 const endpointUrl = "/todos/";
 
 describe(endpointUrl, () => {
-    it("POST" + endpointUrl, () => {
+    it("POST" + endpointUrl, async () => {
         const response = await request(app)
-        .post(endpointUrl)
-        .send(newTodo);
+            .post(endpointUrl)
+            .send(newTodo);
         expect(response.statusCode).toBe(201);
         expect(response.body.title).toBe(newTodo.title);
         expect(response.body.done).toBe(newTodo.done);
